4bad06b926b15f9325e4729e0676b50d08ac27c8
Azhandi Usemahu <azhandi@fore.coffee>
2023-09-21 01:58:08 +0700

    [AAN] update middleware to handle custom error

--- core/middleware.go
+++ core/middleware.go
@@ -1,11 +1,37 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func loadMidleWares(app *fiber.App) {
-	// DATABASE CONNECTION
-	app.Use(recover.New())
+
+	// Custom middleware to recover from panics and send a custom error response
+	app.Use(func(c *fiber.Ctx) error {
+		defer func() {
+			// route := c.Route()
+			// fmt.Println(route.Path)
+			if r := recover(); r != nil {
+				// Recovered from a panic, send a custom error response
+				errorMessage := fmt.Sprintf("%v", r) // Create a custom error message
+				c.Status(fiber.StatusInternalServerError).JSON(CustomErrorResponse{
+					Status:     "error",
+					StatusCode: fiber.StatusInternalServerError,
+					Payload: map[string]interface{}{
+						"error": errorMessage, // Include the custom error message
+					},
+				})
+			}
+		}()
+		return c.Next()
+	})
+
+}
+
+type CustomErrorResponse struct {
+	Status     string      `json:"status"`
+	StatusCode int         `json:"statusCode"`
+	Payload    interface{} `json:"payload"`
 }
