789cb7234e8c1bfe651213688281e93f2c3a7b65
Azhandi Usemahu <azhandi@fore.coffee>
2023-09-22 22:18:55 +0700

    [AAN] remove unused files

--- migrations/20230921124143_create_table_user.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user 
-(
-    id              INT(11)         NOT NULL        AUTO_INCREMENT,
-    name            VARCHAR(255)    NOT NULL,
-    age             INT(11)         NOT NULL        DEFAULT 0,
-    created_date    DATETIME        NOT NULL,
-    updated_date    DATETIME,
-    PRIMARY KEY (id)
-)ENGINE = InnoDB;
