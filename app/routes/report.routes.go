64e5107652f834c55de486a28f5c843f558abc8e
TuantypoZzz <farhan.arfa.fa@gmail.com>
2025-07-23 14:12:52 +0900

    [HAN] CRU Report update

--- app/routes/report.routes.go
+++ app/routes/report.routes.go
@@ -2,10 +2,15 @@ package routes
 
 import (
 	"tampayang-backend/app/controllers"
+	"tampayang-backend/core/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func ReportRoute(app *fiber.App) {
-	app.Post("/reports/", controllers.CreateReports)
+	app.Post("/reports/", controllers.CreateReport)
+	app.Get("/reports/urgently-report/", middlewares.AuthCookie, controllers.UrgencyReport)
+	app.Get("/reports/manage-report/", middlewares.AuthCookie, controllers.ManageReport)
+	app.Get("/reports/detail-report/", middlewares.AuthCookie, controllers.DetailReport)
+	app.Put("/reports/update-report/:report_id", middlewares.AuthCookie, controllers.UpdateReport)
 }
